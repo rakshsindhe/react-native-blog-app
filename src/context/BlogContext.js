478de7b9e749ed39218ba5eff2ac07f545304232
rakshsindhe <35263835+rakshsindhe@users.noreply.github.com>
2020-12-20 13:29:47 +0530

    Added User Reducer with Context without code refactoring

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,20 +1,31 @@
-import React, { Children, createContext } from "react";
+import React, { useReducer, createContext } from "react";
 
 const BlogContext = createContext();
 
-const blogPosts = [{
-    id:1,
-    title:"Blog1"
-},{
-    id:2,
-    title:"Blog2"
-}, {
-    id:3,
-    title:"Blog3"
-}]
+const blogReducer = (state, action) => {
+  const { type, payload } = action;
+  const { title } = payload;
+  switch (type) {
+    case "ADD_BLOG":
+      return [...state, { title }];
+
+    default:
+      state;
+  }
+};
 
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>;
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
+
+  const addBlogPost = blogPost => {
+    dispatch({ type: "ADD_BLOG", payload: blogPost });
+  };
+
+  return (
+    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+      {children}
+    </BlogContext.Provider>
+  );
 };
 
 export default BlogContext;
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,16 +1,47 @@
-import React, { useContext } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { FlatList } from "react-native-gesture-handler";
+import React, { useContext, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Keyboard
+} from "react-native";
+import { FlatList, TextInput } from "react-native-gesture-handler";
 import BlogContext from "../context/BlogContext";
 
 const IndexScreen = () => {
-  const blogPosts = useContext(BlogContext);
+  const { data = [], addBlogPost } = useContext(BlogContext);
+  const [title, setTitle] = useState("");
+
+  const handleBlogPostSubmit = () => {
+    addBlogPost({ title });
+    setTitle("");
+  };
+
   return (
-    <View>
-      <Text>Index Screen</Text>
+    <View style={{ padding: 20 }}>
+      <Text>Add Blog Post</Text>
+      <TextInput
+        style={{ borderWidth: 1, padding: 10, fontSize: 17, marginBottom: 8 }}
+        value={title}
+        onChangeText={setTitle}
+        onEndEditing={() => Keyboard.dismiss()}
+      />
+      <TouchableOpacity
+        style={{
+          padding: 12,
+          backgroundColor: "#3498DB",
+          borderWidth: 1.5,
+          borderColor: "#3498DB",
+          width: 150
+        }}
+        onPress={handleBlogPostSubmit}
+      >
+        <Text style={{ fontSize: 18, textAlign: "center" }}>Add Blog</Text>
+      </TouchableOpacity>
       <FlatList
-        data={blogPosts}
-        keyExtractor={blog => blog.id}
+        data={data}
+        keyExtractor={blog => blog.title}
         renderItem={({ item }) => {
           return <Text>{item.title}</Text>;
         }}
